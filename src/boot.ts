de6aad61100e979b34399e231a2e0fbb75a8c761
Pierre Goudjo <pierre@goudjo.xyz>
2021-04-19 00:21:00 +0200

    Render application inside an element with id=name

--- src/boot.ts
+++ src/boot.ts
@@ -1,37 +1,36 @@
 import type {FunctionComponent} from 'preact'
-import type {Option} from "fp-ts/lib/Option"
-import {AnyAction, configureStore} from "@reduxjs/toolkit"
+import {configureStore, Dispatch, Reducer} from "@reduxjs/toolkit"
 import {render} from "preact"
 import { html } from 'htm/preact'
-import {connect, Matching, Provider} from "react-redux"
+import {connect, Provider} from "react-redux"
 
 
-export type Render<State> = FunctionComponent<State>
+export type Render<S> = FunctionComponent<S>
 
-export type Update<T, Action> = (state: T, action: Action) => T
-export type Optional<T> = Option<T>
+export type Update<S> = Reducer<S>
 
-export const app = <T>(name: string, initialState: T, renderFn: Render<T>, updateFn: Update<T, AnyAction>) => {
+export type Trigger = Dispatch<any>
+export const app = <T, A>(name: string, initialState: T, renderFn: Render<T>, updateFn: Update<T>) => {
   const store = configureStore<T>({
     preloadedState: initialState,
     reducer: updateFn
   })
 
-
   const ConnectedElement = connect(
     (state: T) => state,
     {}
   )(renderFn as any)
-  render(html
-    `
-      <${Provider} store=${store}>
-        <${ConnectedElement} />
-      <//>
-    `
-    , document.body
-  )
-
-  // render(html`<${renderFn} />`, document.body)
+  const element = document.getElementById(name)
+  if (element) {
+    render(html
+      `
+        <${Provider} store=${store}>
+          <${ConnectedElement} />
+        <//>
+      `
+      , element
+    )
+  } else {
+    console.error(`Element of id ${name} doesnt exist`)
+  }
 }
-
-export {}
