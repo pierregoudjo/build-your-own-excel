2ec4fa10bd7e8ec83986ffe63e52d509c9506fcf
Pierre Goudjo <pierre@goudjo.xyz>
2021-04-24 23:50:25 +0200

    Replace redux by unistore

--- src/boot.ts
+++ src/boot.ts
@@ -1,15 +1,25 @@
 import type {FunctionComponent} from 'preact'
-import {configureStore, Dispatch, Reducer} from "@reduxjs/toolkit"
+import type {ActionMap} from 'unistore'
+
+// import {configureStore, Dispatch, Reducer} from "@reduxjs/toolkit"
 import {render} from "preact"
 import { html } from 'htm/preact'
-import {connect, Provider} from "react-redux"
+
+import createStore from 'unistore'
+import { Provider, connect } from 'unistore/preact'
+import devtools from 'unistore/devtools'
+
+// import {connect, Provider} from "react-redux"
 
 
 export type Render<S> = FunctionComponent<S>
 
-export type Update<S> = Reducer<S>
+// export type Update<S> = Reducer<S>
+export type Update<S> = ActionMap<S>
 
-export type Trigger = Dispatch<any>
+
+// export type Trigger = Dispatch<any>
+export type Trigger = (s: any) => any
 
 export const add = (a: number, b: number) => a + b
 export const multiply = (a: number, b: number) => a * b
@@ -18,22 +28,23 @@ export const divide = (a: number, b: number) => a / b
 
 export const numericRange = (start:number, stop: number) => Array.from({length: (stop - start) + 1}, (_, i) => start + i)
 export const alphabeticRange = (start: string, stop: string) => numericRange(start.charCodeAt(0), stop.charCodeAt(0)).map( x => String.fromCharCode(x))
-export const app = <T, A>(name: string, initialState: T, renderFn: Render<T>, updateFn: Update<T>) => {
-  const store = configureStore<T>({
-    preloadedState: initialState,
-    reducer: updateFn
-  })
+export const app = <T, A>(name: string, initialState: T, renderFn: any, updateFn: ActionMap<T>) => {
+  // const store = configureStore<T>({
+  //   preloadedState: initialState,
+  //   reducer: updateFn
+  // })
 
-  const ConnectedElement = connect(
-    (state: T) => state,
-    {}
-  )(renderFn as any)
+
+
+  const store = devtools(createStore(initialState))
+
+  // const ConnectedElement = connect((state: T) => state, updateFn) (renderFn)
   const element = document.getElementById(name)
   if (element) {
     render(html
       `
         <${Provider} store=${store}>
-          <${ConnectedElement} />
+          <${renderFn} />
         <//>
       `
       , element
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import "preact/debug"
 
 import type {Render, Update, Trigger} from "./boot"
+import type {FunctionComponent} from 'preact'
 import type {Option} from "fp-ts/es6/Option"
 import type { Parser } from "parsimmon"
 
-import { createAction, createReducer } from "@reduxjs/toolkit"
-import {none, match, some} from "fp-ts/es6/Option"
+import {none, match, some, fromNullable, toUndefined} from "fp-ts/es6/Option"
+import { pipe } from "fp-ts/es6/function"
 import {html} from "htm/preact"
 import {add, divide, minus, multiply, alphabeticRange, numericRange} from "./boot"
 import {app} from "./boot"
-import { pipe } from "fp-ts/lib/function"
-import { useDispatch } from "react-redux"
-
 import P from "parsimmon"
-
+import { connect } from "unistore/preact"
 
 //
 // Step 1: Define early domain model
@@ -45,6 +43,7 @@ import P from "parsimmon"
 type Position = [string, number]
 const positionAsString = (position: Position) => `${position[0]}${position[1]}`
 const positionEquals = (p1: Position, p2: Position) => p1[0] === p2[0] && p1[1] === p2[1]
+const positionFromColAndRow = (col: string, row: number): Position => ([col, row])
 
 type State = {
   cols: string[]
@@ -93,9 +92,16 @@ const evaluate = (expr: Expr): Value => {
   }
 }
 
-
-const startEdit = createAction<Position, "START_EDIT">("START_EDIT")
-const updateValue = createAction<[Position, string], "UPDATE_VALUE">("UPDATE_VALUE")
+const actions = {
+  startEdit: (_: State, payload: Position) => {
+    return { active:some(payload) }
+  },
+  updateValue: (state: State, payload: [Position, string]) => {
+    const [position, value] = payload
+    const stringPosition = positionAsString(position)
+    return {cells: {...state.cells, [stringPosition]: value}}
+  }
+}
 
 const initial: State = {
   cols: alphabeticRange("A", "Z"),
@@ -104,12 +110,12 @@ const initial: State = {
   cells: {}
 }
 
-const renderEditor = (value: string, position: Position, trigger: Trigger) => {
+const renderEditor = (value: Option<string>, position: Position, updateValue: Trigger) => {
   return html`
     <td class="selected">
      <input 
-      value=${value} 
-      oninput=${(e: { target: HTMLInputElement; }) => trigger(updateValue([position, e.target.value]))} />
+     value=${toUndefined(value)} 
+      onblur=${(e: { target: HTMLInputElement; }) => updateValue([position, e.target.value])} />
     </td>
   `
 }
@@ -122,39 +128,54 @@ const renderView = (value: Option<string>, position: Position, trigger: Trigger)
       (v) => expr.map(evaluate).tryParse(v).toString()
     )
   )
-
+  
   return html`
-    <td onclick=${() => trigger(startEdit(position))}>${displayValue}</td>
+    <td onclick=${() => trigger(position)}>${displayValue}</td>
   `
 }
 
-const renderCell = (state: State, position: Position, trigger: Trigger) =>{
-  const stringPosition = positionAsString(position)
-  const cell = state.cells[stringPosition]
-
-  const cellValue = cell ? some(cell) : none
-
-  return pipe(
-    state.active, 
-    match(
-      () => renderView(cellValue, position, trigger),
-      (active) => positionEquals(active, position) ? renderEditor(cell, position, trigger) : renderView(cellValue, position, trigger)
-    )
-  )
+type CellProps = {
+  position: Position
+  active: boolean
+  cell: Option<string>
+  startEdit: (payload: Position) => void
+  updateValue: (payload: [Position, string]) => void
 }
 
-const App: Render<State> = (props) => {
-  const trigger = useDispatch()
+const Cell: FunctionComponent<CellProps> = ({position, cell, active, startEdit, updateValue}) =>{
+  return active ? renderEditor(cell, position, updateValue) : renderView(cell, position, startEdit)
+}
+
+const ConnectedCell = connect(
+  (state: State, props: Pick<CellProps, 'position'>) => ({
+    cell: fromNullable(state.cells[positionAsString(props.position)]), 
+    active: pipe(
+      state.active,
+      match(
+        () => false,
+        (activeCellPosition) => positionEquals(activeCellPosition, props.position)
+      )
+    )
+  }), 
+  actions 
+) (Cell as any)
+
+type GridProps = {
+  cols: string[]
+  rows: number[]
+}
+
+const Grid: FunctionComponent<GridProps> = ({cols, rows}) => {
   return (
     html`<table>
       <tr>
         <th></th>
-        ${props.cols.map(col => html`<th>${col}</th>`)}
+        ${cols.map(col => html`<th>${col}</th>`)}
       </tr>
-      ${props.rows.map(row => html`
+      ${rows.map(row => html`
         <tr>
-          <th>${row}</th>
-          ${props.cols.map(col => renderCell(props, [col, row], trigger))}
+          <th>${row}</th>  
+          ${cols.map(col =>html`<${ConnectedCell} position=${positionFromColAndRow(col, row)} />`)}
         </tr>
       `)}
     </table>`
@@ -162,16 +183,6 @@ const App: Render<State> = (props) => {
 };
 
 
-const update: Update<State> = createReducer(initial, (builder) => {
-  builder.addCase(startEdit, (state, action) => ({...state, active:some(action.payload)}))
-  builder.addCase(updateValue, (state, action) => {
-    const [position, value] = action.payload
-    const stringPosition = positionAsString(position)
-    return {...state, cells: {...state.cells, [stringPosition]: value}}
-  })
-})
-
-
-app ("main", initial, App, update)
+app ("main", initial, connect(['cols', 'rows' ], {}) (Grid as any), actions)
 
 
